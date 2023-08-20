907a18d2569d1dee46b777b8bda1390c631fdc30
carpetmaker3162 <domesticatedhuman9999@gmail.com>
2023-08-20 17:01:38 -0400

    load images in higher quality

--- utils/img.py
+++ utils/img.py
@@ -1,20 +1,13 @@
-import os
 import pygame
+from PIL import Image
 
 cache = {}
 
-# rotation ?
-# `rot` param
-
-
 def get_image(imagepath: str, width=100, height=100):
     image_key = f"{imagepath.strip()} width={width} height={height}"
-    if image_key in cache:
-        # logs.log(f"using cached '{imagepath}' (width={width} height={height})")
-        pass
-    else:
-        # logs.log(f"loading '{imagepath}' (width={width} height={height})")
-        image = pygame.image.load(imagepath).convert_alpha()
-        image = pygame.transform.scale(image, (width, height))
+    if image_key not in cache:
+        image = Image.open(imagepath).convert("RGBA")
+        image = image.resize((width, height), resample=Image.LANCZOS)
+        image = pygame.image.fromstring(image.tobytes(), image.size, image.mode)
         cache[image_key] = image
     return cache[image_key]
